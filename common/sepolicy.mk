ee3718c4b3f1b5744ed41f4a2413c6c733049383
Anay Wadhera <anay1018@gmail.com>
2022-02-09 17:43:48 +0900

    sepolicy: Include flipendo/turbo_adapter policy
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -1,2 +1,12 @@
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     device/halcyon/sepolicy/common/private
+
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    hardware/google/pixel-sepolicy/flipendo \
+    hardware/google/pixel-sepolicy/turbo_adapter
+else
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    hardware/google/pixel-sepolicy/flipendo \
+    hardware/google/pixel-sepolicy/turbo_adapter
+endif
