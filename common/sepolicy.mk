a5d0ddced5fea387f704a54cce7728ab36c876aa
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-08-20 08:50:50 +0530

    sepolicy: common: Add support for LMOFreeform service
    Signed-off-by: HeroBuxx <me@herobuxx.me>

---
+++ common/sepolicy.mk
@@ -0,0 +1,2 @@
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    device/halcyon/sepolicy/common/private
--- sepolicy.mk
+++ sepolicy.mk
@@ -2,3 +2,6 @@
 # Copyright (C) 2025 Halcyon Project
 # SPDX-License-Identifier: Apache-2.0
 #
+
+# Include common SEPolicy rules
+$(eval include device/halcyon/sepolicy/common/sepolicy.mk)
