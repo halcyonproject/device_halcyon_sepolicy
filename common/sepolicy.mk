b1ac0ced4ebc9dfb9ff9c7af9eed579aceb5bcfb
Ido <dev.xyzuniverse@gmail.com>
2026-07-08 09:59:34 +0200

    common/sepolicy: Use default flipendo coredomain to system_ext * Ref: https://android-review.googlesource.com/c/device/google/gs201-sepolicy/+/2671075 * Fixes: Forbidden attribute coredomain assigned to domain "flipendo" in File   "out/soong/.intermediates/system/sepolicy/contexts/vendor_seapp_contexts/android_common/rodin/vendor_seapp_contexts_m4out" on line 46
    Change-Id: I4949f80b9f8afd135dc457456b586d0c7b9cd62f
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -7,12 +7,6 @@ endif
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     device/halcyon/sepolicy/common/private
 
-ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     hardware/google/pixel-sepolicy/flipendo \
     hardware/google/pixel-sepolicy/turbo_adapter
-else
-BOARD_VENDOR_SEPOLICY_DIRS += \
-    hardware/google/pixel-sepolicy/flipendo \
-    hardware/google/pixel-sepolicy/turbo_adapter
-endif
